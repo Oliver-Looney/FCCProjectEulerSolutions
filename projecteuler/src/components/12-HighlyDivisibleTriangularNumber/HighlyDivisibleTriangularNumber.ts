706b56a915dd5009229c4cb529cb95d3b0eeeea5
Oliver-Looney <oliverjlooney@gmail.com>
2021-08-09 18:05:46 +0100

    new working solution, calculates divisors of coprimes that make up triangular numbers

--- projecteuler/src/components/12-HighlyDivisibleTriangularNumber/HighlyDivisibleTriangularNumber.ts
+++ projecteuler/src/components/12-HighlyDivisibleTriangularNumber/HighlyDivisibleTriangularNumber.ts
@@ -13,14 +13,35 @@ export function getNthTriangularNumber(N: number) {
 }
 
 
-export function HighlyDivisibleTriangularNumberFunc(input: number) {
-    let index = 1;
-    while (true) {
-
-        const currentTriangularNum = getNthTriangularNumber(index);
-        if (getNumOfDivisors(currentTriangularNum) > input) {
-            return currentTriangularNum;
+function getNumOfDivisorsOfABProduct(numOfDivisorsOfA: number, numOfDivisorsOfB: number) {
+    numOfDivisorsOfA = numOfDivisorsOfA - 1;
+    numOfDivisorsOfB = numOfDivisorsOfB - 1;
+    let count = 0;
+    for (let i = 0; i < numOfDivisorsOfA; i++) {
+        for (let j = 0; j < numOfDivisorsOfB; j++) {
+            count++;
         }
-        index++;
     }
+    return count;
+}
+
+export function HighlyDivisibleTriangularNumberFunc(input: number) {
+    let index = 0;
+    let numOfDivisorsOfTriNum = 0;
+    let numOfDivisorsOfA = 0;
+    let numOfDivisorsOfB = 0;
+    do {
+        index++;
+        if (index % 2 === 0) {
+            numOfDivisorsOfA = getNumOfDivisors(index / 2);
+            numOfDivisorsOfB = getNumOfDivisors(index + 1);
+        } else {
+            numOfDivisorsOfA = getNumOfDivisors(index);
+            numOfDivisorsOfB = getNumOfDivisors((index + 1) / 2);
+        }
+
+        numOfDivisorsOfTriNum = numOfDivisorsOfA + numOfDivisorsOfB - 1 + getNumOfDivisorsOfABProduct(numOfDivisorsOfA, numOfDivisorsOfB);
+    }
+    while (numOfDivisorsOfTriNum < input);
+    return getNthTriangularNumber(index)
 }
