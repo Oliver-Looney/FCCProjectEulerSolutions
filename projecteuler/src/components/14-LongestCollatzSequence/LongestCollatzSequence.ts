84a0b2bce5010fcf311fa3e92218db15a3ed9e1c
Oliver-Looney <oliverjlooney@gmail.com>
2021-08-09 22:35:16 +0100

    Storing Collatz numbers in Map completed

--- projecteuler/src/components/14-LongestCollatzSequence/LongestCollatzSequence.ts
+++ projecteuler/src/components/14-LongestCollatzSequence/LongestCollatzSequence.ts
@@ -1,3 +1,21 @@
 export function LongestCollatzSequenceFunc(limit: number) {
-    return -1;
+    let previousCollatzAttempts = new Map<number, number>();
+    previousCollatzAttempts.set(1, 1);
+    previousCollatzAttempts.set(2, 2);
+    if (!previousCollatzAttempts.has(5)) {
+        let n = 5;
+        let count = 0;
+        while (!previousCollatzAttempts.has(n)) {
+            if (n % 2 === 0) {
+                n = n / 2;
+            } else {
+                n = (3 * n) + 1;
+            }
+            count++;
+        }
+        // @ts-ignore
+        previousCollatzAttempts.set(5, previousCollatzAttempts.get(n) + count);
+    }
+
+    return previousCollatzAttempts;
 }
