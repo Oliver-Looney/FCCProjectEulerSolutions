9c897c5b50ed5ba30573c7f41b2f7adf600c0f01
Oliver-Looney <oliverjlooney@gmail.com>
2021-08-09 23:04:17 +0100

    Passes my tests, but not in FCC browser

--- projecteuler/src/components/14-LongestCollatzSequence/LongestCollatzSequence.ts
+++ projecteuler/src/components/14-LongestCollatzSequence/LongestCollatzSequence.ts
@@ -2,20 +2,34 @@ export function LongestCollatzSequenceFunc(limit: number) {
     let previousCollatzAttempts = new Map<number, number>();
     previousCollatzAttempts.set(1, 1);
     previousCollatzAttempts.set(2, 2);
-    if (!previousCollatzAttempts.has(5)) {
-        let n = 5;
-        let count = 0;
-        while (!previousCollatzAttempts.has(n)) {
-            if (n % 2 === 0) {
-                n = n / 2;
-            } else {
-                n = (3 * n) + 1;
+    for (let i = 1; i < limit; i++) {
+        if (!previousCollatzAttempts.has(i)) {
+            let n = i;
+            let count = 0;
+            while (!previousCollatzAttempts.has(n)) {
+                if (n % 2 === 0) {
+                    n = n / 2;
+                } else {
+                    n = (3 * n) + 1;
+                }
+                count++;
             }
-            count++;
+            // @ts-ignore
+            previousCollatzAttempts.set(i, previousCollatzAttempts.get(n) + count);
         }
-        // @ts-ignore
-        previousCollatzAttempts.set(5, previousCollatzAttempts.get(n) + count);
     }
+    return getKeyOfMaxValue(previousCollatzAttempts);
+}
 
-    return previousCollatzAttempts;
+export function getKeyOfMaxValue(myMap: Map<number, number>) {
+    let maxValue = 0;
+    let keyOfMaxValue = 0;
+    // @ts-ignore
+    for (let [key, value] of myMap) {
+        if (value > maxValue) {
+            maxValue = value;
+            keyOfMaxValue = key;
+        }
+    }
+    return keyOfMaxValue;
 }
