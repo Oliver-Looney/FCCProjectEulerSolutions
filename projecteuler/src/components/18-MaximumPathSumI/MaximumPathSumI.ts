db5f3741198a39f139528c8cf2106fac716b5436
Oliver-Looney <oliverjlooney@gmail.com>
2021-08-12 22:12:05 +0100

    working solution - ref: https://lucidmanager.org/data-science/project-euler-18/, go backwards

--- projecteuler/src/components/18-MaximumPathSumI/MaximumPathSumI.ts
+++ projecteuler/src/components/18-MaximumPathSumI/MaximumPathSumI.ts
@@ -1,6 +1,19 @@
 export function MaximumPathSumIFunc(triangle: number[][]) {
 
-    return -1;
+    for (let i = triangle.length - 1; i > 0; i--) {
+        for (let j = 0; j < triangle.length - 2; j++) {
+            if (triangle[i][j + 1] !== 0) {
+                const left = triangle[i - 1][j] + triangle[i][j];
+                const right = triangle[i - 1][j] + triangle[i][j + 1];
+                if (left > right) {
+                    triangle[i - 1][j] = left;
+                } else {
+                    triangle[i - 1][j] = right;
+                }
+            }
+        }
+    }
+    return triangle[0][0];
 }
 
 const testTriangle = [[3, 0, 0, 0],
