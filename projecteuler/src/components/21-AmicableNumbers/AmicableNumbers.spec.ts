fd747e36247de0f50cea7fde15f56bbb7a5034e2
Oliver-Looney <oliverjlooney@gmail.com>
2021-08-13 22:13:53 +0100

    Modified getNumOfDivisors from problem 12 to getSumOfdivisors & the map checker func too, tested both

--- projecteuler/src/components/21-AmicableNumbers/AmicableNumbers.spec.ts
+++ projecteuler/src/components/21-AmicableNumbers/AmicableNumbers.spec.ts
@@ -1,4 +1,4 @@
-import {AmicableNumbersFunc} from "./AmicableNumbers";
+import {AmicableNumbersFunc, getSumOfDivisors} from "./AmicableNumbers";
 
 import assert from "assert";
 
@@ -14,5 +14,20 @@ describe("FCC tests", () => {
     });
     it("10000=>31626", () => {
         assert.equal(AmicableNumbersFunc(10000), 31626);
+    });
+});
+
+describe("Testing get sum of divisors", () => {
+    it("Testing 6", () => {
+        assert.equal(getSumOfDivisors(6), 6);
+    });
+    it("Testing 10", () => {
+        assert.equal(getSumOfDivisors(10), 8);
+    });
+    it("Testing 220", () => {
+        assert.equal(getSumOfDivisors(220), 284);
+    });
+    it("Testing 284", () => {
+        assert.equal(getSumOfDivisors(284), 220);
     });
 });
--- projecteuler/src/components/21-AmicableNumbers/AmicableNumbers.ts
+++ projecteuler/src/components/21-AmicableNumbers/AmicableNumbers.ts
@@ -1,3 +1,29 @@
 export function AmicableNumbersFunc(n: number) {
+    let mapOfNumbersToSumOfProperDivisors = new Map<number, number>();
+    for (let i = 0; i < n; i++) {
+        mapOfNumbersToSumOfProperDivisors = testIfInMap(mapOfNumbersToSumOfProperDivisors, i);
+    }
+    
     return -1;
+}
+
+export function getSumOfDivisors(testNumber: number) {
+    let sum = 0;
+    for (let i = 1; i < Math.sqrt(testNumber); i++) {
+        if (testNumber % i === 0) {
+            sum += i;
+        }
+    }
+    if (testNumber % Math.sqrt(testNumber) === 0) {
+        sum += Math.sqrt(testNumber);
+    }
+    return sum;
+}
+
+
+export function testIfInMap(previousCalc: Map<number, number>, numberToCalc: number) {
+    if (!previousCalc.has(numberToCalc)) {
+        previousCalc.set(numberToCalc, getSumOfDivisors((numberToCalc)));
+    }
+    return previousCalc;
 }
