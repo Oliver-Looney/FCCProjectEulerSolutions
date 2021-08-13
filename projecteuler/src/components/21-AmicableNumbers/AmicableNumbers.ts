1ea3e6f459381e44c3808b63d236a9ca4cd9828a
Oliver-Looney <oliverjlooney@gmail.com>
2021-08-14 00:37:51 +0100

    working Solution

--- projecteuler/src/components/21-AmicableNumbers/AmicableNumbers.ts
+++ projecteuler/src/components/21-AmicableNumbers/AmicableNumbers.ts
@@ -1,25 +1,34 @@
-export function AmicableNumbersFunc(n: number) {
+function getMapOfNumbersBelowN(n: number) {
     let mapOfNumbersToSumOfProperDivisors = new Map<number, number>();
     for (let i = 0; i < n; i++) {
         mapOfNumbersToSumOfProperDivisors = testIfInMap(mapOfNumbersToSumOfProperDivisors, i);
     }
-    
-    return -1;
+    return mapOfNumbersToSumOfProperDivisors;
+}
+
+export function AmicableNumbersFunc(n: number) {
+    let mapOfNumbersToSumOfProperDivisors = getMapOfNumbersBelowN(n);
+    let result = 0;
+    for (let i = 0; i < n; i++) {
+        const checkI = mapOfNumbersToSumOfProperDivisors.get(i);
+        if (mapOfNumbersToSumOfProperDivisors.get(<number>checkI) === i) {
+            if (checkI !== i) {
+                result = result + <number>checkI + i;
+            }
+        }
+    }
+    return result / 2;
 }
 
 export function getSumOfDivisors(testNumber: number) {
     let sum = 0;
-    for (let i = 1; i < Math.sqrt(testNumber); i++) {
+    for (let i = 1; i <= (testNumber / 2); i++) {
         if (testNumber % i === 0) {
             sum += i;
         }
     }
-    if (testNumber % Math.sqrt(testNumber) === 0) {
-        sum += Math.sqrt(testNumber);
-    }
     return sum;
 }
-
 
 export function testIfInMap(previousCalc: Map<number, number>, numberToCalc: number) {
     if (!previousCalc.has(numberToCalc)) {
