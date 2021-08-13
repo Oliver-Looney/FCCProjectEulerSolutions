9d1c2041123aa74e63f16d4ed0c119edd1e5b04e
Oliver-Looney <oliverjlooney@gmail.com>
2021-08-13 21:27:27 +0100

    Completed solution, passed FCC tests using BigInt library

--- projecteuler/src/components/20-FactorialDigitSum/FactorialDigitSum.ts
+++ projecteuler/src/components/20-FactorialDigitSum/FactorialDigitSum.ts
@@ -1,50 +1,17 @@
-export function getNumberAsNumberArray(inputNumber: number) {
-    const tempStringArray = inputNumber.toString().split("").reverse();
-    let result = [];
-    for (let i = 0; i < tempStringArray.length; i++) {
-        result.push(parseInt(tempStringArray[i]));
-    }
-    return result;
-}
-
-export function addAB(result: number[], iArray: number[]) {
-    let carry = 0;
-    for (let i = 0; i < iArray.length; i++) {
-        const tempAdd = result[i] + iArray[i] + carry;
-        if (tempAdd < 10) {
-            carry = 0;
-            result[i] = tempAdd;
-        } else {
-            result[i] = tempAdd % 10;
-            carry = 1;
-        }
-    }
-    if (carry === 1) {
-        result.push(carry);
-    }
-    return result;
-}
-
-function multiplyAxB(oldRes: number[], iArray: number[]) {
-    let result = oldRes;
-    for (let i = 0; i < iArray.length; i++) {
-        result = addAB(result, iArray);
-    }
-    return [-1];
-}
+import bigInt from "big-integer";
 
 function getFactorial(n: number) {
-    let res = [1];
+    let res = bigInt(1);
     for (let i = n; i > 1; i--) {
-        const iArray = getNumberAsNumberArray(i);
-        res = multiplyAxB(res, iArray);
+        let j = bigInt(i);
+        res = res.multiply(j);
     }
-    return res;
+    return res.toString();
 }
 
 
 export function FactorialDigitSumFunc(n: number) {
-    let res = getFactorial(n).toString();
+    let res = getFactorial(n);
     let total = 0;
     for (let i = 0; i < res.length; i++) {
         total += parseInt(res[i]);
