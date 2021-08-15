94d80dc77e364b251edb85605ef8dc151379a810
Oliver-Looney <oliverjlooney@gmail.com>
2021-08-15 11:55:20 +0100

    Able to get array of abundant numbers

--- projecteuler/src/components/23-NonAbunduntSums/NonAbunduntSums.spec.ts
+++ projecteuler/src/components/23-NonAbunduntSums/NonAbunduntSums.spec.ts
@@ -1,6 +1,34 @@
-import {NonAbunduntSumsFunc} from "./NonAbunduntSums";
+import {getAbundantNumbersUpToN, getSumOfDivisors, NonAbunduntSumsFunc} from "./NonAbunduntSums";
 import assert from "assert";
 
+
+describe("Testing getAbundantNumbers", () => {
+   it("11=>[]", () => {
+      const result = getAbundantNumbersUpToN(11);
+      assert.equal(result.length, 0);
+   });
+   it("13=>[12]", () => {
+      const result = getAbundantNumbersUpToN(13);
+      assert.equal(result.length, 1);
+      assert.equal(result[0], 12);
+   });
+});
+
+
+describe("Testing get sum of divisors", () => {
+   it("Testing 6", () => {
+      assert.equal(getSumOfDivisors(6), 6);
+   });
+   it("Testing 10", () => {
+      assert.equal(getSumOfDivisors(10), 8);
+   });
+   it("Testing 220", () => {
+      assert.equal(getSumOfDivisors(220), 284);
+   });
+   it("Testing 284", () => {
+      assert.equal(getSumOfDivisors(284), 220);
+   });
+});
 
 describe("FCC Tests", () => {
    it("10000=>3731004", () => {
--- projecteuler/src/components/23-NonAbunduntSums/NonAbunduntSums.ts
+++ projecteuler/src/components/23-NonAbunduntSums/NonAbunduntSums.ts
@@ -1,4 +1,25 @@
-export function NonAbunduntSumsFunc(input: number) {
+export function getSumOfDivisors(testNumber: number) {
+    let sum = 1;
+    for (let i = 2; i <= Math.sqrt(testNumber); i++) {
+        if (testNumber % i === 0) {
+            sum += i;
+            sum += testNumber / i;
+        }
+    }
+    return sum;
+}
 
+export function getAbundantNumbersUpToN(limit: number) {
+    let abundantNumbers: number[] = [];
+    for (let i = 12; i < limit; i++) {
+        if (getSumOfDivisors(i) > i) {
+            abundantNumbers.push(i);
+        }
+    }
+    return abundantNumbers;
+}
+
+export function NonAbunduntSumsFunc(input: number) {
+    const abundantNumbers = getAbundantNumbersUpToN(input + 1);
     return -1;
 }
