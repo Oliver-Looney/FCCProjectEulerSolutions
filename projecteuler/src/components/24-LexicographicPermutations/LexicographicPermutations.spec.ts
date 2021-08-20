e1fcd87a8a91b3ba680c717240a60ed903088170
Oliver-Looney <oliverjlooney@gmail.com>
2021-08-20 19:25:13 +0100

    wrote and tested getFirstElementOfNthPerm - this is the logic to get the first digit of the desired permutation, this logic just needs generalised to work for all digits of the desired permutation.

--- projecteuler/src/components/24-LexicographicPermutations/LexicographicPermutations.spec.ts
+++ projecteuler/src/components/24-LexicographicPermutations/LexicographicPermutations.spec.ts
@@ -1,5 +1,25 @@
-import {LexicographicPermutationsFunc} from "./LexicographicPermutations";
+import {getFactorial, getFirstElementOfNthPerm, LexicographicPermutationsFunc} from "./LexicographicPermutations";
 import assert from "assert";
+
+
+describe("Testing get first element of nth Perm", () => {
+    it("5,[0,1,2]=>2", () => {
+        assert.equal(getFirstElementOfNthPerm(5, [0, 1, 2]), 2)
+    });
+    it("699999,[0,1,2,3,4,5,6,7,8,9]=>1", () => {
+        assert.equal(getFirstElementOfNthPerm(699999, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), 1)
+    });
+});
+
+describe("Testing get factorial", () => {
+    it("3=>6", () => {
+        assert.equal(getFactorial(3), 6);
+    });
+    it("4=>24", () => {
+        assert.equal(getFactorial(4), 24);
+    });
+});
+
 
 describe("FCC Tests", () => {
     it("699999=>1938246570", () => {
--- projecteuler/src/components/24-LexicographicPermutations/LexicographicPermutations.ts
+++ projecteuler/src/components/24-LexicographicPermutations/LexicographicPermutations.ts
@@ -2,3 +2,16 @@ export function LexicographicPermutationsFunc(input: number) {
 
     return -1;
 }
+
+export function getFirstElementOfNthPerm(input: number, elements: number[]) {
+    return elements[Math.round(input / getFactorial(elements.length - 1)) - 1];
+}
+
+export function getFactorial(start: number) {
+    let sum = 1;
+    while (start > 1) {
+        sum *= start;
+        start--;
+    }
+    return sum;
+}
