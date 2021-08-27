9f5c363bbd693708af9844750d2ab7992a7b3ad9
Oliver-Looney <oliverjlooney@gmail.com>
2021-08-27 21:20:07 +0100

    Factored getting first digit logic into function in reattempt at problem

--- projecteuler/src/components/24-LexicographicPermutations/LexicographicPermutations.ts
+++ projecteuler/src/components/24-LexicographicPermutations/LexicographicPermutations.ts
@@ -1,18 +1,41 @@
 export function LexicographicPermutationsFunc(input: number) {
     let elements = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let currentIndex: number;
     let result = "";
-    currentIndex = getNextElementOfNthPerm(input, elements);
-    result += elements[currentIndex].toString();
-    elements = elements.filter(num => num !== currentIndex);
-    currentIndex = getNextElementOfNthPerm(input, elements);
-    result += elements[currentIndex % elements.length].toString();
-    return parseInt(result);
+    let j: number;
+
+    let lengthOfElements = elements.length;
+    for (let i = 0; i < elements.length; i++) {
+        j = countFirstXPerms(input, getFactorial(elements.length - i - 1));
+        return j;
+    }
+
+    return (parseInt(result));
+}
+
+function countFirstXPerms(input: number, numberOfPerms: number) {
+    let count = 0;
+    do {
+        input -= numberOfPerms;
+        count++;
+    }
+    while (input > numberOfPerms);
+    return count;
 }
 
 export function getNextElementOfNthPerm(input: number, elements: number[]) {
-    const index = Math.round(input / getFactorial(elements.length - 1)) - 1;
+    const index = Math.trunc((input - 1) / getFactorial(elements.length - 1));
     return index;
+}
+
+function removeElement(elements: number[], indexToRemove: number) {
+    let newElements = [];
+    for (let i = 0; i < elements.length; i++) {
+        if (i !== indexToRemove) {
+            newElements.push(elements[i]);
+        }
+    }
+
+    return newElements;
 }
 
 export function getFactorial(start: number) {
